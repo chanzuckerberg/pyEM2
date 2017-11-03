a49a8c99d0692fdf6b81a537b23069dde489c22a
Marcus Kinsella <mkinsella@chanzuckerberg.com>
2017-11-03 09:40:57 -0700

    Make changes to setup.py
    There are a few things:
    
    1. Include a couple important flags from Paolo: msse4.2 and O3 instead of O2
    2. Attempt to find hdf5 and eigen3 rather than hard-coding their locations/names
    3. Change the dev build changes to a simple flag to indicate whether you should
    build all of EM2 or just link against a library that's already built.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
+from ctypes.util import find_library
 import glob
 import os
+import pkgconfig
 import setuptools
 import sys
 
@@ -7,37 +9,54 @@ from setuptools.command.build_ext import build_ext
 
 __version__ = "0.0.1"
 
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
+BOOST_LIBRARIES = ["boost_system", "boost_filesystem", "boost_chrono",
+                   "boost_regex"]
 
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
+IS_DEV_BUILD = False
 
-    def __init__(self, user=False):
-        self.user = user
+def check_build_dependencies():
+    """Check that required libraries are present before trying to build."""
+    for library in BOOST_LIBRARIES:
+        if not find_library(library):
+            raise Exception("Could not find {} library".format(library))
 
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
+    if (not pkgconfig.exists("hdf5")) and (not find_library("hdf5")):
+        raise Exception("Could not find hdf5")
 
-ext_modules = [
-    setuptools.Extension(
-        'pyEM2',
-        sources=glob.glob(os.path.join("src", "pyEM2*.cpp")) + glob.glob("ExpressionMatrix2/src/*.cpp"),
-        include_dirs=[
-            get_pybind_include(),
-            get_pybind_include(user=True),
-            os.path.join("ExpressionMatrix2", "src"),
-            "/usr/include/hdf5/serial",
-            "/usr/include/eigen3",
-            "/usr/local/include/eigen3"
-        ],
-        libraries=["boost_system", "boost_filesystem", "boost_regex",
-                   "hdf5_cpp", "hdf5", "boost_python-py34",
-                   "boost_chrono"],
-        language="c++")
-]
+    if not pkgconfig.exists("eigen3"):
+        raise Exception("Could not find hdf5")
+
+def get_libraries():
+    """Get libraries for building EM2.
+
+    This is mostly just finding hdf5. Boost doesn't seem to work with
+    pkg-config.
+    """
+
+    all_libraries = list(BOOST_LIBRARIES)
+    if pkgconfig.exists("hdf5"):
+        all_libraries.extend(pkgconfig.parse("hdf5")['libraries'])
+    else:
+        all_libraries.append("hdf5")
+    all_libraries.append("hdf5_cpp")
+
+    return all_libraries
+
+def get_library_dirs():
+    """Get any library dirs to pass to the compiler."""
+
+    library_dirs = pkgconfig.parse("hdf5")['library_dirs']
+    return library_dirs
+
+def get_include_dirs():
+    """Get includes to pass to the compiler."""
+    include_dirs = []
+    include_dirs.extend(pkgconfig.parse("eigen3")["include_dirs"])
+    include_dirs.extend(pkgconfig.parse("hdf5")["include_dirs"])
+    include_dirs.append(get_pybind_include())
+    include_dirs.append(get_pybind_include(user=True))
+    return include_dirs
+
 
 def has_flag(compiler, flagname):
     """Return a boolean indicating whether a flag name is supported on
@@ -66,6 +85,19 @@ def cpp_flag(compiler):
         raise RuntimeError('Unsupported compiler -- at least C++11 support '
                            'is needed!')
 
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
 
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
@@ -77,7 +109,15 @@ class BuildExt(build_ext):
     if sys.platform == 'darwin':
         c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
 
+
     def build_extensions(self):
+        # Avoid lots of warnings
+        try:
+            self.compiler.compiler_so.remove("-Wstrict-prototypes")
+        except ValueError:
+            pass
+
+
         ct = self.compiler.compiler_type
         opts = self.c_opts.get(ct, [])
         if ct == 'unix':
@@ -87,9 +127,32 @@ class BuildExt(build_ext):
                 opts.append('-fvisibility=hidden')
         elif ct == 'msvc':
             opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
+
+        # Opts for building EM2 from Paolo
+        try:
+            self.compiler.compiler_so.remove("-O2")
+        except ValueError:
+            pass
+        if has_flag(self.compiler, '-msse4.2'):
+            opts.append("-msse4.2")
+        opts.append("-O3")
+
         for ext in self.extensions:
             ext.extra_compile_args = opts
         build_ext.build_extensions(self)
+
+check_build_dependencies()
+
+ext_modules = [
+    setuptools.Extension(
+        'pyEM2',
+        sources=glob.glob(os.path.join("src", "pyEM2*.cpp")) +
+        (glob.glob("ExpressionMatrix2/src/*.cpp") if not IS_DEV_BUILD else []),
+        include_dirs=get_include_dirs() + [os.path.join("ExpressionMatrix2", "src")],
+        library_dirs=(["ExpressionMatrix2"] if IS_DEV_BUILD else []) + get_library_dirs(),
+        libraries=(["em2"] if IS_DEV_BUILD else []) + get_libraries(),
+        language="c++")
+]
 
 setuptools.setup(
     name='pyEM2',
